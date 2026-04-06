ced97f19ca65ab2aef5b885b1972c62a149b6722
Alfonso Hernandez <alfonso.hernandez@astian.org>
2026-04-06 02:51:19 -0500

    chore: add ESLint + Prettier with Vue 3 and TypeScript support
    Installs eslint, typescript-eslint, eslint-plugin-vue, prettier,
    and eslint-config-prettier. Adds lint, lint:fix, and format scripts.
    Ref: T22

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js'
+import ts from 'typescript-eslint'
+import vue from 'eslint-plugin-vue'
+import prettier from 'eslint-config-prettier'
+
+export default ts.config(
+  { ignores: ['dist/'] },
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: { parser: ts.parser },
+    },
+  },
+  prettier,
+)
